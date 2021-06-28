4a81f302f0b3f1c11f3f9823b472dea43d9f7684
hoangnguyen424331 <kingstyle424331@gmail.com>
2021-06-28 17:05:17 +0700

    unit-test DetailMovie

--- app/src/main/java/com/example/appmovie/utils/FakeData.kt
+++ app/src/main/java/com/example/appmovie/utils/FakeData.kt
@@ -1,10 +1,12 @@
 package com.example.appmovie.utils
 
+import com.example.appmovie.data.model.DetailMovie
 import com.example.appmovie.data.model.SearchMovie
 
 object FakeData {
     const val QUERY_SEARCH = "nguyen"
     const val PAGE_SEARCH = 1
+    const val ID_MOVIE_DETAIL = 445571
 
     val SEARCH_MODEL = mutableListOf<SearchMovie?>(
         SearchMovie(
@@ -13,5 +15,17 @@ object FakeData {
         SearchMovie(
             396371, "Molly's Game", "path 2"
         )
+    )
+
+    val DETAIL_MOVIE_MODEL = DetailMovie(
+        "abc",
+        "posterUrl",
+        0,
+        mutableListOf(),
+        "title",
+        "overview",
+        "tagline",
+        "releaseDate",
+        3.5
     )
 }
